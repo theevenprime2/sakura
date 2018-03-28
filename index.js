1c2d962f2ba2c7b27f416b5baacb44d8908277fc
Jordan Chen <jordantchen@gmail.com>
2018-03-28 02:27:53 -0500

    super barebones

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-'use strict';
+// 'use strict';
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const http = require('http');
+// const http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser');
-const slackEventsApi = require('@slack/events-api');
-const { WebClient } = require('@slack/client');
+// const bodyParser = require('body-parser');
+// const slackEventsApi = require('@slack/events-api');
+// const { WebClient } = require('@slack/client');
 
-const SlackClient = require('./slackClient');
-const { registerEvents } = require('./slackEvents');
+// const SlackClient = require('./slackClient');
+// const { registerEvents } = require('./slackEvents');
 
 // const slackEventsAdapter = slackEventsApi.createSlackEventAdapter('vNtGGM4nuwF36koKluSlM8Dt', { includeBody: true });
 // const slackWebClient = new WebClient('xoxp-327667997619-327667997651-337075952069-e627b7d02f60ff0d63b7b1176e1cb487');
@@ -24,6 +24,7 @@ const app = express();
 // app.use('/slack/events', slackClient.events.expressMiddleware());
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/bye', (req, res) => res.send('bye!'))
 
 app.listen(port, () => console.log(`zServer listening on port ${port}`));
 

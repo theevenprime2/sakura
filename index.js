293bc0b128b02256a3617b85270d9aad408125ec
Jordan Chen <jordantchen@gmail.com>
2018-03-28 03:18:52 -0500

    3000

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const express = require('express');
 
 // registerEvents(slackClient);
 
-const port = 20005;
+const port = process.env.PORT || 3000;
 const app = express();
 
 // app.use(bodyParser.json());

03d14578031faf0f1b368848f996efa2d5996703
Jordan Chen <jordantchen@gmail.com>
2018-03-28 02:21:23 -0500

    test barebones

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ const { WebClient } = require('@slack/client');
 const SlackClient = require('./slackClient');
 const { registerEvents } = require('./slackEvents');
 
-const slackEventsAdapter = slackEventsApi.createSlackEventAdapter('vNtGGM4nuwF36koKluSlM8Dt', { includeBody: true });
-const slackWebClient = new WebClient('xoxp-327667997619-327667997651-337075952069-e627b7d02f60ff0d63b7b1176e1cb487');
-const slackClient = new SlackClient(slackEventsAdapter, slackWebClient);
+// const slackEventsAdapter = slackEventsApi.createSlackEventAdapter('vNtGGM4nuwF36koKluSlM8Dt', { includeBody: true });
+// const slackWebClient = new WebClient('xoxp-327667997619-327667997651-337075952069-e627b7d02f60ff0d63b7b1176e1cb487');
+// const slackClient = new SlackClient(slackEventsAdapter, slackWebClient);
 
-registerEvents(slackClient);
+// registerEvents(slackClient);
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(bodyParser.json());
-app.use('/slack/events', slackClient.events.expressMiddleware());
+// app.use(bodyParser.json());
+// app.use('/slack/events', slackClient.events.expressMiddleware());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
